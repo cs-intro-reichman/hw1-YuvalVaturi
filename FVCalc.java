bb811c7127cc33fd69cd798dc18f08580679701b
YuvalVaturi <yuvalvaturi1@gmail.com>
2024-11-07 14:27:32 +0200

    bill1

--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,11 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
 	public static void main(String[] args){
-		// Replace this comment with your code
+		int currentValue = Integer.parseInt(args[0]);
+		int rate = Double.parseDouble(String);//Integer.parseInt(args[1]);
+		int n = Integer.parseInt(args[2]);
+		perc = rate/100;
+		int savings = currentValue*Math.pow((1+rate),n);
+		System.out.println("After " + n + " years, " + "$" + currentValue + " saved at " + rate + "% will yield $" + ((int)savings));
 	}
 }
