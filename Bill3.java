3ddf19092c424e9f559a139d3f73af8ae719a85a
YuvalVaturi <yuvalvaturi1@gmail.com>
2024-11-07 16:58:44 +0200

    biil2

--- Bill3.java
+++ Bill3.java
@@ -4,9 +4,9 @@ public class Bill3 {
 		String name1 = args[0];
 		String name2 = args[1];
 		String name3 = args[2];
-		int price = Integer.parseInt(args[3]);
-		int split = (int) Math.ceil(price/3.0);
-		System.out.println("Dear " + name1 + "," + name2 + " and " + name3 + ": Pay " + split + " Shekels each."); //
+		double price = Integer.parseInt(args[3]);
+		double split = (int) Math.ceil(price/3.0);
+		System.out.println("Dear " + name3 + ", " + name2 + " and " + name1 + ": pay " + split + " Shekels each."); //
 	}
 }
-
+// java Bill3.java Ron Lisa Dan 100
